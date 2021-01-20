da92c0515a2460c239d0282aef820e913f0865e8
Baibhav Sinha <baibhav1750.cse18@chitkara.edu.in>
2021-01-20 23:43:55 +0530

    Update Abstraction Student question.java

--- Abstraction Student question.java
+++ Abstraction Student question.java
@@ -1,1 +1,9 @@
+class Source {
+    public static void main(String[] args) {
 
+    // Create an object named 's1' and complete the code to print the desired output
+    Student s1 = new Student(1056,"karan",8.8);
+    s1.displayProfile();
+    System.out.println("Percentage of s1 is "+s1.findPercentage());
+    }
+}
