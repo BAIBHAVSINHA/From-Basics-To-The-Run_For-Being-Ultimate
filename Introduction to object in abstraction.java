7fc45496170e42ff577d3d5d3f6d304dfa79b920
Baibhav Sinha <baibhav1750.cse18@chitkara.edu.in>
2021-01-20 23:41:46 +0530

    Update Introduction to object in abstraction.java

--- Introduction to object in abstraction.java
+++ Introduction to object in abstraction.java
@@ -1,1 +1,10 @@
-
+class Source{
+    public static void main(String[] a){
+    //Create two objects of Square class 's1' and 's2'
+    Square s1 = new Square(10);
+    Square s2 = new Square(20);
+    //Calculate and print their areas after assigning values to their side lengths
+    System.out.println(s1.area());
+    System.out.println(s2.area());
+    }
+}
