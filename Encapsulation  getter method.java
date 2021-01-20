27ab7c765b2d34b8c7a11b252ba2a402ea550ad0
Baibhav Sinha <baibhav1750.cse18@chitkara.edu.in>
2021-01-20 23:46:25 +0530

    Update Encapsulation  getter method.java

--- Encapsulation  getter method.java
+++ Encapsulation  getter method.java
@@ -1,1 +1,8 @@
-
+class Source {
+   public static void main(String[] args) {
+   
+       // Write code here
+       Person H = new Person("Ankit");
+       System.out.println(H.getName());
+   }
+}
