0590b6d6254b2b16cd024f1f3981c25c2c4c1d64
Baibhav Sinha <baibhav1750.cse18@chitkara.edu.in>
2021-01-21 21:42:42 +0530

    Update Swapping.java

--- Swapping.java
+++ Swapping.java
@@ -1,1 +1,16 @@
+import java.util.*;
+public class Source {
 
+   public static void main(String[] args) {
+
+      int a,b;
+      a=1;
+      b=2;
+      int c;
+      c=a;
+      a=b;
+      b=c;
+      System.out.println("a="+a);
+      System.out.println("b="+b);
+   }
+}
