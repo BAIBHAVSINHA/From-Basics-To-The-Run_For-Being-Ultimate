6e1a2a894b40da7c0beadcf309fd9a15b0056565
Baibhav Sinha <baibhav1750.cse18@chitkara.edu.in>
2021-01-21 21:51:32 +0530

    Update Celsius to fahrenheit.java

--- Celsius to fahrenheit.java
+++ Celsius to fahrenheit.java
@@ -1,1 +1,13 @@
+import java.util.*;
+public class Source {
+
+   public static void main(String[] args) {
+
+      int C, F;
+      Scanner input= new Scanner(System.in);
+      C=input.nextInt();
+      F=9*C/5+32;
+      System.out.println(F);
+   }
+}
 
