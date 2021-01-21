438d0a9e950cc5a10ff12f56ad6409143f8861fe
Baibhav Sinha <baibhav1750.cse18@chitkara.edu.in>
2021-01-21 22:05:20 +0530

    Update Boolean Datatype.java

--- Boolean Datatype.java
+++ Boolean Datatype.java
@@ -1,1 +1,18 @@
-
+import java.util.Scanner;
+class Source {
+  public static void main(String[] args) {
+    int myAge;
+    int ageOfBrother;
+    Scanner sc = new Scanner(System.in);
+    myAge = sc.nextInt();
+    ageOfBrother = sc.nextInt();
+    if(myAge<25 && ageOfBrother<25)
+    {
+        System.out.print("true");
+    }
+    else
+    {
+        System.out.print("false");
+    }
+  }
+}
