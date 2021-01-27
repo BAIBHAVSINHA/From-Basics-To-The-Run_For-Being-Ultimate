cd22e502f0fd0c27bc46638c63b952a97a56a7ce
Baibhav Sinha <baibhav1750.cse18@chitkara.edu.in>
2021-01-27 23:55:48 +0530

    Update Assignment1.java

--- Assignment1.java
+++ Assignment1.java
@@ -7,3 +7,12 @@ import java.util.Queue;
 import java.util.Scanner;
 
 class Road {
+ public String city1;
+    public String city2;
+
+    public Road(String city1, String city2) {
+        this.city1 = city1;
+        this.city2 = city2;
+    }
+
+}
